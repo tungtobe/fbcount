a898aa48739a46e29c8a3ed1926b93e63bdacd61
TobeTung <tungnt@codergarden.org>
2016-02-21 11:12:57 +0900

    edit .env.example and fix example login FB

--- app/Http/Controllers/FacebookController.php
+++ app/Http/Controllers/FacebookController.php
@@ -41,51 +41,53 @@ class FacebookController extends Controller
 	    // dd($token);
 	    
 
-	    // // Access token will be null if the user denied the request
-	    // // or if someone just hit this URL outside of the OAuth flow.
-	    // if (! $token) {
-	    //     // Get the redirect helper
-	    //     $helper = $fb->getRedirectLoginHelper();
+	    // Access token will be null if the user denied the request
+	    // or if someone just hit this URL outside of the OAuth flow.
+	    if (! $token) {
+	        // Get the redirect helper
+	        $helper = $fb->getRedirectLoginHelper();
 
-	    //     if (! $helper->getError()) {
-	    //         abort(403, 'Unauthorized action.');
-	    //     }
+	        if (! $helper->getError()) {
+	            abort(403, 'Unauthorized action.');
+	        }
 
-	    //     // User denied the request
-	    //     dd(
-	    //         $helper->getError(),
-	    //         $helper->getErrorCode(),
-	    //         $helper->getErrorReason(),
-	    //         $helper->getErrorDescription()
-	    //     );
-	    // }
+	        // User denied the request
+	        dd(
+	            $helper->getError(),
+	            $helper->getErrorCode(),
+	            $helper->getErrorReason(),
+	            $helper->getErrorDescription()
+	        );
+	    }
 
-	    // if (! $token->isLongLived()) {
-	    //     // OAuth 2.0 client handler
-	    //     $oauth_client = $fb->getOAuth2Client();
+	    if (! $token->isLongLived()) {
+	        // OAuth 2.0 client handler
+	        $oauth_client = $fb->getOAuth2Client();
 
-	    //     // Extend the access token.
-	    //     try {
-	    //         $token = $oauth_client->getLongLivedAccessToken($token);
-	    //     } catch (\Facebook\Exceptions\FacebookSDKException $e) {
-	    //         dd($e->getMessage());
-	    //     }
-	    // }
+	        // Extend the access token.
+	        try {
+	            $token = $oauth_client->getLongLivedAccessToken($token);
+	        } catch (\Facebook\Exceptions\FacebookSDKException $e) {
+	            dd($e->getMessage());
+	        }
+	    }
 
-	    // $fb->setDefaultAccessToken($token);
+	    $fb->setDefaultAccessToken($token);
+
 
 	    // // Save for later
 	    // \Session::put('fb_user_access_token', (string) $token);
 
-	    // // Get basic info on the user from Facebook.
-	    // try {
-	    //     $response = $fb->get('/me?fields=id,name,email');
-	    // } catch (\Facebook\Exceptions\FacebookSDKException $e) {
-	    //     dd($e->getMessage());
-	    // }
+	    // Get basic info on the user from Facebook.
+	    try {
+	        $response = $fb->get('/me?fields=id,name,email');
+	    } catch (\Facebook\Exceptions\FacebookSDKException $e) {
+	        dd($e->getMessage());
+	    }
 
-	    // // Convert the response to a `Facebook/GraphNodes/GraphUser` collection
-	    // $facebook_user = $response->getGraphUser();
+	    // Convert the response to a `Facebook/GraphNodes/GraphUser` collection
+	    $facebook_user = $response->getGraphUser();
+	    dd($facebook_user->getName());
 
 	    // // Create the user if it does not exist or update the existing entry.
 	    // // This will only work if you've added the SyncableGraphNodeTrait to your User model.
